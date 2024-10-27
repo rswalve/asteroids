c3f1495f96b3a0da377b53d7424c5cf9a08414be
“rswalve” <robert.swalve@gmail.com>
2024-10-26 22:26:19 -0500

    Changes after lesson 3-2

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, other_shape):
+        distance = self.position.distance_to(other_shape.position)
+        radii = self.radius + other_shape.radius
+        return distance <= radii
+
