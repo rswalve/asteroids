462cd15dbe0b2abf8f3ba8a8a10998f029a9fd0d
“rswalve” <robert.swalve@gmail.com>
2024-11-08 18:41:03 -0600

    Added final changes

--- main.py
+++ main.py
@@ -18,10 +18,12 @@ updatable = pygame.sprite.Group()
 drawable = pygame.sprite.Group()
 asteroids = pygame.sprite.Group()
 shots_group = pygame.sprite.Group()
+shots = pygame.sprite.Group()
 
 Player.containers = (updatable, drawable)
 Asteroid.containers = (asteroids, updatable, drawable)
 AsteroidField.containers = (updatable,)
+Shot.containers = (shots, updatable, drawable)
 
 
 player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, shots_group)
@@ -53,6 +55,11 @@ def main():
             if player.collision(asteroid):
                 print("Game Over")
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.split()
 
         # Render
         screen.fill((0, 0, 0))  # Clear the screen
